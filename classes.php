209102faf9c818ebda0bb3532426a6ea69d76451
Javier1995 <quiroga218@gmail.com>
2018-07-15 08:09:08 -0500

    Excelete codigo para las clase que se desean compartir

---
+++ classes.php
@@ -0,0 +1,26 @@
+<?php 
+
+class Person
+{
+
+    public $age;
+    public $name;
+    public function run() 
+    {
+            return ' está corriendo';
+    }
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+
+}
+
+$person = new Person('Javier', 20);
+
+echo $person->name;
+
+echo $person->run();
