ad02781a524160602c11af09392a3c46e45207d8
Javier1995 <quiroga218@gmail.com>
2018-07-29 18:48:27 -0500

    Clase para excepciones de errores;

---
+++ exceptions.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/* try{
+  
+  throw new Exception("No hay excepciones");
+
+}catch(Exception $e)
+{
+  echo 'Hubo un error: '. $e->getMessage();
+
+}finally
+{
+  echo "<br />Cerrando BD";
+} */
+  
+class DBException extends Exception{}
+
+class Main 
+{
+  public function __construct()
+  {
+    try
+    {
+      throw new DBException(" No hay conexion ");
+    }catch(DBException $e)
+    {
+      echo 'Error especifico: '. $e->getMessage()."<br />";
+    }
+  }
+}
+$main = new Main();
